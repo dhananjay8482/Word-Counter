90b46abda065cb11926a216e6ec7109d4feee411
Dhananjay Sonar <sonardjay24.official@gmail.com>
2022-08-09 11:39:06 +0530

    word Counter Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import arrayWords from "../utils/words"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-genertor';
+
+  words = ''
+
+  limit = 10;
+
+
+  handleSlideChange(newLimit : number){
+    this.limit = newLimit;
+  }
+
+  generate(){
+    this.words = arrayWords.slice(0,this.limit).join(" ");
+  }
+
+  
+  
+
+
+
 }
